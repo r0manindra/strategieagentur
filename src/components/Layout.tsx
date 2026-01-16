7d315c4a0d7df2a935299b13bcc6e50c784f06f7
r0manindra <romanindra1@gmail.com>
2026-01-16 10:17:03 +0100

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
-import { motion, AnimatePresence } from 'framer-motion'
 import Header from './Header'
 import Footer from './Footer'
 
@@ -14,18 +13,9 @@ const Layout = () => {
   return (
     <>
       <Header />
-      <AnimatePresence mode="wait">
-        <motion.main
-          key={location.pathname}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.3 }}
-          style={{ paddingTop: '72px' }}
-        >
-          <Outlet />
-        </motion.main>
-      </AnimatePresence>
+      <main style={{ paddingTop: '72px' }}>
+        <Outlet />
+      </main>
       <Footer />
     </>
   )
